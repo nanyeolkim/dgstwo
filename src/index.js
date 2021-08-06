d557193c98aa2cce998fc2b2337e26010cf120e3
김난열 <nanyeolkim@gimnan-yeol-ui-MacBookAir.local>
2021-08-06 11:07:56 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Run the server on a port specified in our .env file or port 4000
+const port = process.env.PORT || 4000;
+
+// Construct a schema, using GraphQL's schema language
+const typeDefs = gql`
+    type Query {
+        hello: String
+    }
+`;
+
+// Provide resolver functions for our schema fields
+const resolvers = {
+    Query: {
+        hello: () => 'Hello world!'
+    }
+};
+
+const app = express();
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api'});
+
+app.listen({ port }, () =>
+    console.log`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`
+);
+
